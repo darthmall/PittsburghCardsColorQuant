a71e942e566776c8958e97063c59972e49d7ebf3
darthmall <wallace.sheehan@gmail.com>
2012-08-24 20:55:39 -0600

    Write a quick Python script to import the CSV data into CouchDB

---
+++ import.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+
+from argparse import ArgumentParser
+import csv, couchdb
+
+parser = ArgumentParser('Import a CSV file of greeting card color data to CouchDB')
+parser.add_argument('datafile', metavar='CSV')
+parser.add_argument('db', metavar='DB')
+
+args = parser.parse_args()
+
+couch = couchdb.Server('http://esheehan:bogus123@localhost:5984')
+
+if args.db not in couch:
+    couch.create(args.db)
+
+db = couch[args.db]
+
+with file(args.datafile, 'r') as f:
+    reader = csv.reader(f)
+
+    for line in reader:
+        colors = []
+
+        for i in range(1, len(line[1:]), 2):
+            colors.append({
+                'color': line[i],
+                'frequency': line[i + 1]
+                })
+
+        doc = {
+            '_id': line[0],
+            'colors': colors,
+            'date': '{}-{}'.format(line[0][:4], line[0][4:6])
+        }
+
+        db.save(doc)
